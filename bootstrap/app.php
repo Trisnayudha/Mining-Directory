ae2bdcb66391f3e5e43d9357564e7f63d2e84cec
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-05-13 10:40:07 +0700

    fix bug singleton

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -56,4 +56,13 @@ $app->router->group([
     require __DIR__ . '/../routes/web.php';
 });
 
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
 return $app;
