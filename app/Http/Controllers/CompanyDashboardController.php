d9ee46860905a6c98fe76e156353ce149cce7548
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-09-06 09:30:09 +0800

    update bug

--- app/Http/Controllers/CompanyDashboardController.php
+++ app/Http/Controllers/CompanyDashboardController.php
@@ -93,6 +93,6 @@ class CompanyDashboardController extends Controller
         if (!$data) {
             return $this->sendResponse('User not found', [], 404);
         }
-        return $this->sendResponse('Successfully show data', $data, $data['status'] ?? 200);
+        return $this->sendResponse($data['message'] ?? 'Successfully show data', $data, $data['status'] ?? 200);
     }
 }
