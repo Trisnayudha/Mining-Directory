afce5a931ab4a029037f7b8c84aa31f30195c051
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-09-09 20:20:09 +0700

    update company information

--- app/Repositories/Eloquent/CompanyInformationRepository.php
+++ app/Repositories/Eloquent/CompanyInformationRepository.php
@@ -8,6 +8,8 @@ use App\Models\Product;
 use App\Repositories\Contracts\CompanyInformationRepositoryInterface;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
+use Intervention\Image\Facades\Image;
+
 
 class CompanyInformationRepository implements CompanyInformationRepositoryInterface
 {
@@ -29,7 +31,48 @@ class CompanyInformationRepository implements CompanyInformationRepositoryInterf
 
         // Mengecualikan password dari data yang akan di-update
         $data = $request->except('password');
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            if ($file) {
+                $imageName = time() . '.' . $file->extension();
+                $dbPath = 'storage/company/' . $imageName;
+                $saveFolder = $file->storeAs('public/company', $imageName);
 
+                // Create a new Intervention Image instance from the uploaded file
+                $compressedImage = Image::make(storage_path('app/public/company/' . $imageName));
+
+                // Resize the image to a maximum width of 400 while maintaining aspect ratio
+                $compressedImage->resize(400, null, function ($constraint) {
+                    $constraint->aspectRatio();
+                    $constraint->upsize();
+                });
+
+                // Save the resized image
+                $compressedImage->save(storage_path('app/public/company/' . $imageName));
+                $user->image = url($dbPath);
+            }
+        }
+        if ($request->hasFile('banner_image')) {
+            $banner_image = $request->file('banner_image');
+            if ($banner_image) {
+                $imageName = time() . '.' . $banner_image->extension();
+                $dbPathBanner = 'storage/company-banner/' . $imageName;
+                $saveFolder = $banner_image->storeAs('public/company-banner', $imageName);
+
+                // Create a new Intervention Image instance from the uploaded file
+                $compressedImage = Image::make(storage_path('app/public/company-banner/' . $imageName));
+
+                // Resize the image to a maximum width of 400 while maintaining aspect ratio
+                $compressedImage->resize(400, null, function ($constraint) {
+                    $constraint->aspectRatio();
+                    $constraint->upsize();
+                });
+
+                // Save the resized image
+                $compressedImage->save(storage_path('app/public/company-banner/' . $imageName));
+                $user->banner_image = url($dbPathBanner);
+            }
+        }
         // Update data user tanpa password
         $user->update($data);
 
