679968b9c22116c150fa1e9a3befb7af86417bd8
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-09-09 20:23:14 +0700

    update company information

--- app/Repositories/Eloquent/CompanyInformationRepository.php
+++ app/Repositories/Eloquent/CompanyInformationRepository.php
@@ -49,7 +49,7 @@ class CompanyInformationRepository implements CompanyInformationRepositoryInterf
 
                 // Save the resized image
                 $compressedImage->save(storage_path('app/public/company/' . $imageName));
-                $data->image = url($dbPath);
+                $data['image'] = url($dbPath);
             }
         }
         if ($request->hasFile('banner_image')) {
@@ -70,7 +70,7 @@ class CompanyInformationRepository implements CompanyInformationRepositoryInterf
 
                 // Save the resized image
                 $compressedImage->save(storage_path('app/public/company-banner/' . $imageName));
-                $data->banner_image = url($dbPathBanner);
+                $data['banner_image'] = url($dbPathBanner);
             }
         }
         // Update data user tanpa password
